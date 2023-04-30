9082ec35c1180b2bf25b4476fd3a5052b5380af9
Protap Ghosh <protapg518@gmail.com>
2023-04-30 14:20:54 +0600

    find categories id

--- index.js
+++ index.js
@@ -16,9 +16,21 @@ app.get("/categories", (req, res) => {
     res.send(categories);
 });
 
-app.get("news", (req, res) => {
-    res.send(news);
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
 })
+
+app.get("/news", (req, res) => {
+    res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find((n) => n._id === id);
+    res.send(selectedNews);
+});
 
 app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`);
