5b86d072a910da46bd69ebf575f761c502270d88
Protap Ghosh <protapg518@gmail.com>
2023-04-30 14:08:55 +0600

    news data via upload

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -13,6 +15,10 @@ app.get("/", (req, res) => {
 app.get("/categories", (req, res) => {
     res.send(categories);
 });
+
+app.get("news", (req, res) => {
+    res.send(news);
+})
 
 app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`);
